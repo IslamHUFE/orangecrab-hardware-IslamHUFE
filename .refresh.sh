4c682e10b14f2c682331f0b7e3226d37ef90be14
Islam Tarek <islam.my01@gmail.com>
2026-06-11 14:54:32 +0300

    refresh

--- .refresh.sh
+++ .refresh.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 git add .
-
 git commit -m "refresh"
-git push origin main
+
+if ! git push origin main; then
+    echo "pulling last commits from the main repository folder"
+    
+    if git pull --rebase origin main; then
+        echo "you are now up to date"
+
+        git push origin main
+    else
+        echo "Conflict"
+        exit 1
+    fi
+fi
